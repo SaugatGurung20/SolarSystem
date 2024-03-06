77d1e2c703b9f2fed1e57c2a5d27b870fabae077
Saugat Gurung <grgseol2015@gmail.com>
2024-03-06 19:19:01 +0000

    files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import './App.css';
+import ThreeScene from './PlanetThree/ThreeScene';
+
+function App() {
+  return (
+    <div className="App">
+      <ThreeScene />
+    </div>
+  );
+}
+
+export default App;
